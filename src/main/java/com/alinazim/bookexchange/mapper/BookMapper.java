ecd8ee6be2037bd6b7fecce962b31e2babef9e3f
alihuseynaliyev <alihuseyneliyev12@gmail.com>
2023-07-26 11:23:26 +0400

    first commit

--- src/main/java/com/alinazim/bookexchange/mapper/BookMapper.java
+++ src/main/java/com/alinazim/bookexchange/mapper/BookMapper.java
@@ -2,21 +2,18 @@ package com.alinazim.bookexchange.mapper;
 
 import com.alinazim.bookexchange.dao.entity.BookEntity;
 import com.alinazim.bookexchange.dao.entity.UserEntity;
-import com.alinazim.bookexchange.dao.repository.UserRepository;
 import com.alinazim.bookexchange.model.request.BookRequest;
 import com.alinazim.bookexchange.model.response.BookResponse;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @Mapper
 public interface BookMapper {
     BookMapper INSTANCE = Mappers.getMapper(BookMapper.class);
 
-    default BookEntity buildBookEntity(BookRequest bookRequest, Optional<UserEntity> userEntityOptional) {
+    default BookEntity buildBookEntity(BookRequest bookRequest, UserEntity userEntity) {
         if (bookRequest == null) {
             return null;
         }
@@ -27,11 +24,7 @@ public interface BookMapper {
         bookEntity.setAuthor(bookRequest.getAuthor());
         bookEntity.setGenre(bookRequest.getGenre());
         if (bookRequest.getUserId() != null) {
-            if (userEntityOptional.isPresent()) {
-                bookEntity.setUser(userEntityOptional.get());
-            } else {
-                throw new RuntimeException("User not found with id: " + bookRequest.getUserId());
-            }
+            bookEntity.setUser(userEntity);
         }
 
         return bookEntity;
@@ -39,6 +32,7 @@ public interface BookMapper {
 
     BookResponse buildBookResponse(BookEntity bookEntity);
 
+<<<<<<< HEAD
     default List<BookEntity> buildBookEntities(List<BookRequest> bookRequests, UserRepository userRepository) {
         if (bookRequests == null) {
             return null;
@@ -57,6 +51,8 @@ public interface BookMapper {
         return bookEntities;
     }
 
+=======
+>>>>>>> a363a8b (first commit)
     List<BookResponse> buildBookResponses(List<BookEntity> entityList);
 }
 
